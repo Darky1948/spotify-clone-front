d1f5bed8b066ba661a731e0e8bc3124f14ed4f18
Kristen Viguier <kristen.viguier@gmail.com>
2024-10-03 16:11:02 +0200

    Init font awesome and testing it works

---
+++ src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,7 @@
+// This class will contains the icon that we will need in the app.
+import { IconDefinition } from "@fortawesome/free-regular-svg-icons";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomIcons: IconDefinition[] = [
+  faCirclePlay
+];
